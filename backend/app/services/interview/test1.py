1eb6533a292d2cc7f3fca8b49a8b10d6060f2a75
folknawatt <folknawat64@gmail.com>
2026-01-15 13:57:28 +0700

    Delete files

--- backend/app/services/interview/test1.py
+++
@@ -1,7 +0,0 @@
-from app.services.interview.resume_service import ResumeService
-
-
-if __name__ == "__main__":
-    resume_service = ResumeService()
-    resume_service.generate_questions_from_pdf(
-        r"D:\ai-interview\backend\app\services\interview\resume.pdf")
